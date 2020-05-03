20dc82b20a6f7a5cb89413235f9ac52734b56f7b
Waleed Saleh <44371736+waleedts@users.noreply.github.com>
2020-05-03 19:25:59 +0300

    Update Main.java

--- library/Main.java
+++ library/Main.java
@@ -11,16 +11,33 @@ public class Main {
             String [] strArr = scanner.nextLine().split(" ", 4);//To split line to 4 parts
             String command=strArr[0].toLowerCase();//To make command case insensitive
             switch (command){
-                case "add":library.add(strArr[1]);break;
-                case "delete":library.delete(strArr[1]);break;
-                case "set":library.set(strArr[1],strArr[2],strArr[3]);break;
+                case "add":
+                    library.add(strArr[1]);
+                    break;
+                case "delete":
+                    library.delete(strArr[1]);
+                    break;
+                case "set":
+                    library.set(strArr[1],strArr[2],strArr[3]);
+                    break;
                 case "get":
-                    System.out.println(library.get(strArr[1],strArr[2]));break;
-                case "rent": library.rent(strArr[1]);break;
-                case "return":library.returns(strArr[1]);break;
-                case "search": library.search(strArr[1],strArr[2]+" "+strArr[3]);break;
-                case "print":System.out.println(library);break;
-                case "quit":in=false;break;//Exits the loop
+                    System.out.println(library.get(strArr[1],strArr[2]));
+                    break;
+                case "rent": 
+                    library.rent(strArr[1]);
+                    break;
+                case "return":
+                    library.returns(strArr[1]);
+                    break;
+                case "search": 
+                    library.search(strArr[1],strArr[2]+" "+strArr[3]);
+                    break;
+                case "print":
+                    System.out.println(library);
+                    break;
+                case "quit":
+                    in=false;
+                    break;//Exits the loop
                 default:
                     System.err.println("There is no such command as "+command);//if the command is fals
             }
